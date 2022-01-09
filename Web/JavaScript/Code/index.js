9803ac0f07cae2b3322e77a2843e15f24804ae12
Smith Brian <Entired.0@Gmail.com>
2022-01-09 11:33:51 +0700

    update

---
+++ Web/JavaScript/Code/index.js
@@ -0,0 +1,118 @@
+// in-line comment.
+
+// creat variable
+let count = 0
+
+console.log(count)
+
+// 1. Creat a variable, myAge, and set its value to your age and log the myAge variable to the console.
+let myAge = 14
+
+console.log(myAge)
+
+let num = 5 / 7
+
+console.log(num)
+
+let number_num = num
+
+console.log(number_num)
+
+let other_number = 2
+let fin_num = num * number_num + other_number
+
+console.log(fin_num)
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+
+let other_myAge = 14
+let humanDogRatio = 7
+
+let myDogAge = other_myAge * humanDogRatio
+console.log(myDogAge)
+
+let var_1 = 1
+console.log(var_1)
+
+var_1 = 10
+console.log(var_1)
+
+var_1 = var_1 + 12
+console.log(var_1)
+
+var_1 = var_1 - 12
+console.log(var_1)
+
+// Creat a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70.
+
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+
+function increment() {
+    console.log("The button was clicked")
+}
+
+function countdown() {
+    // Setting up the race
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+// GO!
+console.log("GO!")
+
+// Players are running the race
+console.log("Players are running the race")
+
+// Race is finished
+console.log("Race is finished")
+
+// Get ready for a new race
+console.log("Get ready for a new race")
+countdown()
+
+// Creat a function (you decide the name) the logs out the number 42 to the console
+// Call/invoke the function
+
+function function_ex() {
+    console.log("42")
+}
+
+function_ex()
+
+function sum_laps() {
+    let lap1 = 34
+    let lap2 = 33
+    let lap3 = 36
+    console.log(lap1 + lap2 + lap3)
+}
+
+sum_laps()
+
+// Creat a function that inccrements the lapsCompleted variable with one
+// Run it three times
+
+let lapsCompleted = 0
+
+function laps_completed() {
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
+}
+
+let counter = 0
+function count_el() {
+    counter = counter + 1
+    document.getElementById("count-el").innerText = counter
+}
+
