4f47df44faba29cacc1d7f7986fa33c963b818c8
Smith-Br <Entired.0@Gmail.com>
2022-02-27 23:10:17 +0700

    update

--- Web/JavaScript/Javascript/Storing Values with the Assignment Operator.js
+++ Web/JavaScript/Javascript/Storing Values with the Assignment Operator.js
@@ -2,3 +2,7 @@
 undefined, null, boolean, string, symbol, number, and object
 */
 
+console.log("the quick brown fox jumps over the lazy dog")
+
+let some_var = "The quick brown fox jumps over the lazy dog"
+
